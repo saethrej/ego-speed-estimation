8090fb0baeb627ebe3a2301d1190da4001679702
Christopher Raffl <rafflc@student.ethz.ch>
2022-04-22 19:33:19 +0200

    Python script for converting videos to mp4

---
+++ convert_video.py
@@ -0,0 +1,17 @@
+import os 
+
+# filename = "runner_preprocessing.py" 
+filename ="video.hevc"
+outfilename = "video.mp4"
+
+def main():
+    this_dir = os.getcwd()
+    for root, subFolders, files in os.walk(this_dir):
+        if not filename in files:
+            continue
+        #print("This file: " + os.path.join(root, filename))
+        #print("Output file: " + os.path.join(root, outfilename))
+        os.system("ffmpeg -i '" + os.path.join(root, filename) + "' '" + os.path.join(root, outfilename) + "'")
+        #os.remove(os.path.join(root, filename))
+
+main()
