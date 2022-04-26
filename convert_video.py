d4c958ae086711f673db44d7b292a5f77acb77c5
saethrej <saethrej@student.ethz.ch>
2022-04-26 17:23:50 +0200

    added file count in video conversion

--- convert_video.py
+++ convert_video.py
@@ -6,12 +6,22 @@ outfilename = "video.mp4"
 
 def main():
     this_dir = os.getcwd()
+    num_videos = 0
+    num_converted = 0
     for root, subFolders, files in os.walk(this_dir):
+        if filename in files:
+            num_videos += 1
+        if outfilename in files:
+            num_converted += 1
+
         if (not filename in files) or (outfilename in files):
             continue
         #print("This file: " + os.path.join(root, filename))
         #print("Output file: " + os.path.join(root, outfilename))
         os.system("ffmpeg -i '" + os.path.join(root, filename) + "' '" + os.path.join(root, outfilename) + "'")
         #os.remove(os.path.join(root, filename))
+    
+    print("Input Videos:", num_videos)
+    print("Converted Videos:", num_converted)
 
 main()
